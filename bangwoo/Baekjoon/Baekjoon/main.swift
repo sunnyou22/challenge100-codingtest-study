08345d5df7675a089588a7a81e61665c0d045da6
sunnyou22 <sunnyouyaya22@gmail.com>
2022-10-20 20:15:11 +0900

    [BOJ] 에디터 / 연습문제 / 1시간

--- bangwoo/Baekjoon/Baekjoon/main.swift
+++ bangwoo/Baekjoon/Baekjoon/main.swift
@@ -7,21 +7,68 @@
 
 import Foundation
 
-//MARK: 알파벳 개수
+//MARK: 에디터 초반 풀이법
+//var str = readLine()!
+//let cursor = " "
+//let result = str.appending(cursor)
+//var location = 0
+//
+//
+//if readLine()! == "L" {
+//    location += 1
+//
+//    if let a = str.firstIndex(of: " ") {
+//        let index = str.index(a, offsetBy: (location))
+//        location <= str.count ?
+//        str.pop
+//        str.insert(contentsOf: cursor, at: index) : str.insert(contentsOf: cursor, at: str.startIndex)
+//
+//    }
+//} else if readLine()! == "D" {
+//    location -= 1
+//    if let a = str.firstIndex(of: " ") {
+//        let index = str.index(a, offsetBy: (-location))
+//        location <= str.count ? str.insert(contentsOf: cursor, at: index) : str.insert(contentsOf: cursor, at: str.endIndex)
+//    }
+//} else if readLine()! == "B" {
+//    if let a = str.firstIndex(of: " ") {
+//        let index = str.index(a, offsetBy: -1)
+//        str.remove(at: index)
+//    }
+//} else if readLine()!.contains("P"){
+//    if let a = str.firstIndex(of: " ") {
+//        let index = str.index(before: a)
+//        str.insert(contentsOf: readLine()!, at: index)
+//    }
+//}
+//
+//print(str)
 
-let str = readLine()!.sorted()
-let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
-var result = Array(repeating: "0", count: 26)
- 
+//MARK: 에디터
 
-    alphabet.forEach { i in
-      
-        if str.contains(i) {
-            result[alphabet.firstIndex(of: i)!] = "\(str.filter { i == $0 }.count)"
-        } else {
-            result[alphabet.firstIndex(of: i)!] = "0"
+var left = readLine()!
+var right = [Character]()
+var count = Int(readLine()!)!
+
+for _ in 0..<count {
+    let edit = readLine()!
+    switch edit {
+    case "L":
+        if !left.isEmpty {
+            // right에는 마지막 문자열이 삭제되고 남은게 append됨
+            right.append(left.removeLast())
         }
+    case "D":
+        if !right.isEmpty {
+            left.append(right.removeLast())
+        }
+    case "B":
+        if !left.isEmpty {
+            left.removeLast()
+        }
+    default:
+        left.append(edit.last!)
     }
+}
 
-
-print(result.joined(separator: " "))
+print([left + right.reversed()].joined())
